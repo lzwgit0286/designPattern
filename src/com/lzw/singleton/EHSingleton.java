94d82da915398d7e324d018cec4b8b9dbfffd7a9
zhuwu <165442943@qq.com>
2020-03-28 19:17:03 +0800

    单例模式

--- src/com/lzw/singleton/EHSingleton.java
+++ src/com/lzw/singleton/EHSingleton.java
@@ -1,8 +1,16 @@
 package com.lzw.singleton;
 
 /**
+ * 饿汉
  * @author zhuwu.long
  * @Date 2020/3/28 19:02
  */
 public class EHSingleton {
+    private static final EHSingleton INSTANCE = new EHSingleton();
+    private EHSingleton(){
+    }
+
+    public static EHSingleton getInstance(){
+        return INSTANCE;
+    }
 }
--- src/com/lzw/singleton/LHSingleton.java
+++ src/com/lzw/singleton/LHSingleton.java
@@ -1,8 +1,38 @@
 package com.lzw.singleton;
 
 /**
+ * 懒汉
  * @author zhuwu.long
  * @Date 2020/3/28 19:02
  */
 public class LHSingleton {
+    private static volatile LHSingleton INSTANCE = null;
+    private LHSingleton(){
+    }
+    public static LHSingleton getInstance(){
+        if(INSTANCE == null){
+            try{
+                Thread.sleep(200);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            synchronized (LHSingleton.class){
+                if(INSTANCE == null) {
+                    INSTANCE = new LHSingleton();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    public static void main(String[] agrs){
+        for(int i = 0; i < 100; i++){
+            new Thread(
+                    () -> {
+                        System.out.println(getInstance().hashCode());
+                    }
+            ).start();
+        }
+    }
 }
